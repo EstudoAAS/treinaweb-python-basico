9bd8e736208268ee987c58a9fc643c850546b03c
Aller Augusto de Souza <allerasouza@gmail.com>
2017-08-23 18:03:55 -0300

    Add first exercises

---
+++ src/hello_world.py
@@ -0,0 +1,5 @@
+# Isso é um comentário!
+# Esse é outro comentário!
+print("Hello world!")
+palavra = "Treinaweb"
+print(palavra[3:6])
---
+++ src/tipos_numericos.py
@@ -0,0 +1,25 @@
+inteiro_decimal = 123
+print("Inteiro no formato decimal: ")
+print(inteiro_decimal)
+inteiro_hexadecimal = 0x14F
+print("Inteiro no formato hexadecimal: ")
+print(inteiro_hexadecimal)
+print(hex(inteiro_hexadecimal))
+numero_octal = 0o155
+print("Inteiro no formato octal: ")
+print(numero_octal)
+print(oct(numero_octal))
+inteiro_binario = 0b1101
+print("Inteiro no formato binário: ")
+print(inteiro_binario)
+print(bin(inteiro_binario))
+
+flutuante_ponto = 3.14
+print("Número flutuante: ")
+print(flutuante_ponto)
+flutuante_expoente = 3.14e-10
+print("Número flutuante com notação científica: ")
+print(flutuante_expoente)
+flutuante_complexo = 3.14 + 2j
+print("Número flutuante complexo: ")
+print(flutuante_complexo)
